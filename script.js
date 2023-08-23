ea4d82b888f5d8b865c2efe7417453d008d927eb
imvot <110433315+imvot@users.noreply.github.com>
2023-08-23 11:10:58 -0700

    Add operate function

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function divide(a, b) {
     if(b == 0) return "Divsion by Zero"
     return Math.round((a / b)*100) / 100 // Keep last two decimals
 }
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+":
+            add(a, b)
+            break;
+        case "-":
+            substract(a, b)
+            break;
+        case "*":
+            multiply(a, b)
+            break;
+        case "/":
+            divide(a, b)
+            break;
+    }
+}
+
