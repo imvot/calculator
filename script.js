a821bc68d8c4c68c35bd145fa38f0fb36db14648
imvot <110433315+imvot@users.noreply.github.com>
2023-08-25 10:39:48 -0700

    Add addOperator function
    Added addOperator()
    Added number conversions in math functions
    Modified math function calls in operate() for them to actually work
    Added event listener for operator buttons

--- script.js
+++ script.js
@@ -31,6 +31,9 @@ class Calculator {
 
     addNb() {
         const number = this.getAttribute("data-number")
+
+        if(self.currentPos == "operator") self.currentPos = "nb2"
+
         if(self.currentPos == "nb1") {
             self.nb1 += number
         } else if(self.currentPos == "nb2") {
@@ -39,33 +42,49 @@ class Calculator {
         self.updateDisplay()
     }
 
+    addOperator() {
+        const operator = this.getAttribute("data-operator")
+        if(self.nb1 && operator != "=") {
+            self.operator = operator;
+            self.currentPos = "operator";
+            self.updateDisplay()
+        }
+        if(self.nb1 && self.nb2) {
+            self.nb1 = self.operate();
+            self.nb2 = "";
+            self.currentPos = "nb1"
+            self.updateDisplay()
+            self.currentPos = "nb2"
+        }
+    }
+
     add(a, b) {
-        return a + b
+        return +a + +b
     }
 
     substract(a, b) {
-        return a - b
+        return +a - +b
     }
 
     multiply(a, b) {
-        return a * b
+        return +a * +b
     }
 
     divide(a, b) {
         if(b == 0) return "Divsion by Zero"
-        return Math.round((a / b)*100) / 100 // Keep last two decimals
+        return Math.round((+a / +b)*100) / 100 // Keep last two decimals
     }
 
     operate() {
         switch(this.operator) {
             case "+":
-                return add(this.nb1, this.nb2)
+                return this.add(this.nb1, this.nb2)
             case "-":
-                return substract(this.nb1, this.nb2)
+                return this.substract(this.nb1, this.nb2)
             case "*":
-                return multiply(this.nb1, this.nb2)
+                return this.multiply(this.nb1, this.nb2)
             case "/":
-                return divide(this.nb1, this.nb2)
+                return this.divide(this.nb1, this.nb2)
         }
     }
 }
@@ -75,3 +94,6 @@ const calculator = new Calculator(display)
 
 const nbBtns = [...document.querySelectorAll("button[data-number]")]
 nbBtns.forEach(btn => btn.addEventListener("click", calculator.addNb))
+
+const operatorBtns = [...document.querySelectorAll("button[data-operator]")]
+operatorBtns.forEach(btn => btn.addEventListener("click", calculator.addOperator))
